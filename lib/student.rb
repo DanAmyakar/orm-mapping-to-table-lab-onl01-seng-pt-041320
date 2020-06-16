055f027a639bb6b2ae0ebc9cc9377dbf6b31fb44
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-16 16:34:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+  attr_reader :id
+  attr_accessor :name, :grade
   def initialize(id = nil, name, grade)
     @id = id
     @name = name
