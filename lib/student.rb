599c0e6528a51afdee9758cfe6c21a4f22759abe
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-16 16:33:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,36 +8,5 @@ class Student
     @name = name
     @grade = grade
   end
-  
-  def self.create_table
-    sql <<- SQL
-      CREATE TABLE IF NOT EXITS students(
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT)
-        SQL  
-      DB[:conn].execute(sql)
-    end
-  end
-  
-  def self.drop_table
-    sql <<- SQL
-      DROP TABLE students
-      SQL  
-    DB[:conn].execute(sql)
-  end
-  
-  def save
-    sql <<- SQL
-      INSERT INTO students (id, name, grade)
-      VALUES (@id, @name, @grade)
-      SQL
-    DB[:conn].execute(sql, self.id, self.name, self.grade)
-  end
-  
-  def self.create
-    student = self.new(id, name, grade)
-    student.save
-  end
-    
+
 end
