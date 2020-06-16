55d07903111d72ada0624894c67214a6d00cc51b
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-16 16:37:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,18 @@ class Student
     @grade = grade
   end
   
+    
   def self.create_table
-    sql = 
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXITS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT)
+      SQL
+      
+      DB[:conn].execute(sql)
+    end
+  end
+  
+  
 end
